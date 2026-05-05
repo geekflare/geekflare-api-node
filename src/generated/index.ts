30d169a4e2cdb3c18794cfc58351cf2e7f30fcd3
Vaibhav Mishra <vaibhav@geekflare.com>
2026-05-05 11:32:15 +0530

    fix: update package versions and clean up import paths

--- src/generated/index.ts
+++ src/generated/index.ts
@@ -19,7 +19,7 @@ export {
   tlsScan,
   url2Pdf,
   webScrape,
-} from "./sdk.gen.js";
+} from "./sdk.gen";
 export type {
   BaseErrorResponseDto,
   BrokenLinkData,
@@ -186,4 +186,4 @@ export type {
   WebScrapeResponse,
   WebScrapeResponseDto,
   WebScrapeResponses,
-} from "./types.gen.js";
+} from "./types.gen";
