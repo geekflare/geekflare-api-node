587facb00574e5b4c607cbe968ee4c135544bc18
Vaibhav Mishra <vaibhav@geekflare.com>
2026-05-05 10:35:43 +0530

    updated git ignore

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { GeekflareClient } from "./src/index.js";
-
-const client = new GeekflareClient({
-  apiKey: "your-api-key-here",
-  baseUrl: "https://api.geekflare.com",
-});
-
-const result = await client.ping({ url: "https://google.com" });
-console.log(JSON.stringify(result, null, 2));
